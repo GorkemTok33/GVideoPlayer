04c0a214daeeeab1b8de25fd4358a431553761bb
GorkemTok33 <79594881+GorkemTok33@users.noreply.github.com>
2021-05-31 03:58:53 +0300

    Add files via upload

---
+++ GVideoPlayer Qt Source Code/Source Codes/Source Codes/start.cpp
@@ -0,0 +1,28 @@
+/*
+****************************************************************************************
+****************************************************************************************
+Program: GVideoPlayer (Gorkem Video Player)
+Publish Date: 06.15.2021
+Author: Görkem Tok
+Language: C++
+Platform: Qt Creator
+Contact: ceng.gorkem.tok@gmail.com
+License: Creative Commons License, Copyright 2021 Görkem Tok
+****************************************************************************************
+****************************************************************************************
+*/
+
+#include "start.h"
+#include "ui_start.h"
+
+start::start(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::start)
+{
+    ui->setupUi(this);
+}
+
+start::~start()
+{
+    delete ui;
+}
